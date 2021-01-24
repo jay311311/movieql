2ebb5fb1b9cda751ead79413e4d4860b9c941e50
jay311311 <jay311311@gmail.com>
2021-01-25 00:21:03 +0900

    how to use mutation in graphql

--- graphql/db.js
+++ graphql/db.js
@@ -1,43 +1,48 @@
-export const people = [
+ let movies = [
     {
-        id:"0",
-        name:"kim",
-        age:18,
-        gender:"female"
+        id:0,
+        name: "star-wars",
+        score:1
     },{
-        id:"1",
-        name:"E",
-        age:19,
-        gender:"male"
+        id:1,
+        name: "24-identities",
+        score:4
     },
     {
-        id:"2",
-        name:"D",
-        age:38,
-        gender:"male"
+        id:2,
+        name: "alone",
+        score:3
     },
     {
-        id:"3",
-        name:"C",
-        age:28,
-        gender:"female"
-    },{
-        id:"4",
-        name:"B",
-        age:16,
-        gender:"male"
-    },{
-        id:"5",
-        name:"A",
-        age:17,
-        gender:"female"
+        id:3,
+        name: "harry-potter",
+        score:5
     }
-
-
 ];
 
+export const getMovies = () => movies;
 
-export const getById = id=>{
-    const filteredPeople = people.filter(person=> String(id) === person.id);
-    return filteredPeople[0]
+export const getById = () =>{
+    const filteredMovies = movies.filter(movie=> id === movie.id);
+    return filteredMovies[0]
+}
+
+export const deleteMovie =id =>{
+    const cleanMovies = movies.filter(movie => movie.id !== id)
+    if( movies.length > cleanMovies.length){
+        movies =  cleanMovies;
+        return true
+    }else{
+        return false
+    }
+}
+
+export const addMovie = (name, score) =>{
+    const newMovie ={
+        id:`${movies.length + 1}`,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
 }
